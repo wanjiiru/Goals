fb377b3a73228e52cbb07b2120768c64718a7556
wanjiiru <liz240@live.com>
2018-05-22 15:34:17 +0300

    add toggle

--- src/app/goal.ts
+++ src/app/goal.ts
@@ -4,6 +4,15 @@
 // export class Goal {
 //     constructor(public id:number, public name:string){}
 // }
+// export class Goal {
+//     constructor(public id:number, public name:string,public description:string){}
+// }
+
+
 export class Goal {
-    constructor(public id:number, public name:string,public description:string){}
+    public showDescription:boolean;
+    constructor(public id:number, public name:string,public description:string){
+        this.showDescription=false
+
+    }
 }
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -7,20 +7,41 @@ import {Goal} from '../goal'
   templateUrl: './goal.component.html',
   styleUrls: ['./goal.component.css']
 })
-export class GoalComponent implements OnInit {
-  goals = [
-    new Goal(1,'Watch Finding Nemo','Find an online version and watch merlin find his son'),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot'),
-    new Goal(3, 'Get new Phone Case','Diana has her birthday coming up soon'),
-    new Goal(4, 'Get Dog Food','Pupper likes expensive sancks'),
-    new Goal(5, 'Solve math homework','Damn Math'),
-    new Goal(6, 'Plot my world domination plan','Cause I am an evil overlord'),
-]
+// export class GoalComponent implements OnInit {
+//   goals = [
+//     new Goal(1,'Watch Finding Nemo','Find an online version and watch merlin find his son'),
+//     new Goal(2,'Buy Cookies','I have to buy cookies for the parrot'),
+//     new Goal(3, 'Get new Phone Case','Diana has her birthday coming up soon'),
+//     new Goal(4, 'Get Dog Food','Pupper likes expensive sancks'),
+//     new Goal(5, 'Solve math homework','Damn Math'),
+//     new Goal(6, 'Plot my world domination plan','Cause I am an evil overlord'),
+// ]
   
 
-  constructor() { }
+//   constructor() { }
 
-  ngOnInit() {
+//   ngOnInit() {
+//   }
+
+// }
+
+
+export class GoalComponent implements OnInit {
+  goals = [
+      new Goal(1,'Watch Finding Nemo','Find an online version and watch merlin find his son'),
+      new Goal(2,'Buy Cookies','I have to buy cookies for the parrot'),
+      new Goal(3, 'Get new Phone Case','Diana has her birthday coming up soon'),
+      new Goal(4, 'Get Dog Food','Pupper likes expensive sancks'),
+      new Goal(5, 'Solve math homework','Damn Math'),
+      new Goal(6, 'Plot my world domination plan','Cause I am an evil overlord'),
+
+  ]
+
+  toogleDetails(index){
+      this.goals[index].showDescription = !this.goals[index].showDescription;
   }
 
+constructor() { }
+ngOnInit() {
+}
 }
