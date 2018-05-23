90ae27d1419ac79b9732ccdda1ce230757406a35
wanjiiru <liz240@live.com>
2018-05-23 21:39:56 +0300

    add bootstrap and submit functions

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -37,11 +37,36 @@ export class GoalComponent implements OnInit {
 
 ]
 
-  completeGoal(isComplete,index){
-    if (isComplete){
-        this.goals.splice(index,1);
-        }
-        }
+addNewGoal(goal){
+  let goalLength = this.goals.length;
+  goal.id=goalLength+1;
+  goal.completeDate = new Date(goal.completeDate)
+  this.goals.push(goal)
+
+}
+
+
+deleteGoal(isComplete,index){
+  if (isComplete){
+      let toDelete=confirm(`Are you sure you want to delete ${this.goals[index].name}`)
+      
+      if(toDelete){
+          this.goals.splice(index,1)
+      }
+  }
+}
+
+
+
+
+
+
+
+//   completeGoal(isComplete,index){
+//     if (isComplete){
+//         this.goals.splice(index,1);
+//         }
+//         }
 
   toogleDetails(index){
       this.goals[index].showDescription = !this.goals[index].showDescription;
